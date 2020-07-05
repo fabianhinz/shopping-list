21ce711184a2bbb26b1adc2997b7601015e6b360
fabianhinz <hinzfabian.fh@gmail.com>
2020-07-05 15:49:35 +0200

    components

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,5 +1,51 @@
 import React from 'react'
+import {
+    AppBar,
+    Toolbar,
+    Typography,
+    Card,
+    CardHeader,
+    IconButton,
+    CardContent,
+    CardActions,
+    Button,
+    CssBaseline,
+} from '@material-ui/core'
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart'
 
-const App = () => <div>hello world</div>
+const Header = () => (
+    <AppBar position="static">
+        <Toolbar>
+            <Typography variant="h4">OKShopping</Typography>
+        </Toolbar>
+    </AppBar>
+)
+
+const App = () => (
+    <>
+        <Header />
+        <CssBaseline />
+        <Card>
+            <CardHeader
+                title="Cupidatat magna ad"
+                subheader="Duis voluptate quis sint laborum nisi."
+                action={
+                    <IconButton>
+                        <AddShoppingCartIcon />
+                    </IconButton>
+                }
+            />
+            <CardContent>
+                <Typography variant="subtitle1">
+                    Anim deserunt nulla consectetur enim ad cillum incididunt ea velit adipisicing
+                    aute ad.
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button color="secondary">details</Button>
+            </CardActions>
+        </Card>
+    </>
+)
 
 export default App
